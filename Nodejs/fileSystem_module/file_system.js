72906a2bbb5bb5df0ceefc3f986482f900466639
RIYA5561 <agrawalriya868@gmail.com>
2026-01-20 15:57:16 +0530

    riya 20/1/26 work completed

--- Nodejs/fileSystem_module/file_system.js
+++ Nodejs/fileSystem_module/file_system.js
@@ -1,31 +1,48 @@
 // modules
-const file= require("fs") // inbuild module for custom modules we use "./" eg- ./fs
-const path= require("path")
+// const file= require("fs") // inbuild module for custom modules we use "./" eg- ./fs
+// const path= require("path")
 
-const fs= require("http") //
-http.createServer(function(req,res){
+// // const fs= require("http") //
+// // http.createServer(function(req,res){
     
-})
+// // })
 
-const fs=require("fs");
-fs.writeFileSync("./test.txt", "This is Sync file content")
+// const fs=require("fs");
+// fs.writeFileSync("./test.txt", "This is Sync file content")
 
-//Sync
+// //Sync
 
-const file=fs.readFileSync("test.txt","utf-8") // utf-8 is one of the method which is use to decode the file content
-console.log(file)
+// const file=fs.readFileSync("test.txt","utf-8") // utf-8 is one of the method which is use to decode the file content
+// console.log(file)
 
 
-// Async
-const asyncFile=fs.readFile("test.txt","utf-8", (err,data)=>{
+// // Async
+// const asyncFile=fs.readFile("test.txt","utf-8", (err,data)=>{
+//     if(err){
+//         console.log("error in file reading", err);
+
+//     }
+//     else{
+//         console.log("file reading successfull", data)
+//     }
+// })
+// console.log(asyncFile)
+
+// // whenever we write file its content gets overrite so we use append to add content without deleting the old data
+
+
+const fs = require("fs");
+fs.copyFileSync("test.txt","dest.txt")
+fs.copyFile("test.txt","dest_async.txt",(err)=>{
     if(err){
-        console.log("error in file reading", err);
-
-    }
-    else{
-        console.log("file reading successfull", data)
+        console.log("error in copying file", err)
+    }else{
+        console.log("file copied successfully")
     }
 })
-console.log(asyncFile)
+    
 
-// whenever we write file its content gets overrite so we use append to add content without deleting the old data
+
+
+
+
